4a56b93ba6ad820404bdd468d89b880dd4e1eacb
dimitrisstaratzis <jstaratzi@icloud.com>
2023-02-14 00:44:43 +0200

    complete __init__.py

--- src/tiledb/connector/__init__.py
+++ src/tiledb/connector/__init__.py
@@ -1,3 +1,22 @@
 apilevel = "2.0"
 threadsafety = 2
 paramstyle = "pyformat"
+
+from .Cursor import Cursor
+from .Connection import TileDBConnection
+from .Error import (
+    DatabaseError,
+    DataError,
+    Error,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    _Warning,
+)
+
+
+def Connect(**kwargs) -> TileDBConnection:
+    return TileDBConnection(**kwargs)
